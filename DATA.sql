76b6384447a4e877a1fa2c61874b361732a805d3
partickkk <167540991+partickkk@users.noreply.github.com>
2024-10-09 15:12:15 +0800

    Add files via upload

---
+++ DATA.sql
@@ -0,0 +1,131 @@
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (1, 'Ford', 'Freestyle', 2007, 'WBAYA8C55ED804250', 'Yellow', 48325, 'Sold', 1829490);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (2, 'Toyota', 'Avalon', 1997, 'WBA3V7C54FP366281', 'Khaki', 177586, 'Available', 1408196);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (3, 'Chrysler', 'LHS', 1997, 'SALCP2BG2FH772162', 'Yellow', 140963, 'Avaailable', 4044425);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (4, 'MINI', 'Cooper Clubman', 2012, 'WVGAV7AX2FW472292', 'Fuscia', 120286, 'Available', 2709406);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (5, 'Dodge', 'Ram 1500 Club', 1996, 'WA1CGCFEXBD770791', 'Blue', 79067, 'Available', 3516422);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (6, 'Mazda', 'Miata MX-5', 1994, '2FMDK4JC5AB229939', 'Yellow', 16203, 'Pending', 1586119);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (7, 'Nissan', 'Altima', 2006, '19UUA56972A726700', 'Puce', 17804, 'Available', 3105600);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (8, 'Nissan', 'Sentra', 1991, 'WAUMR94E78N402563', 'Aquamarine', 56350, 'Available', 3096924);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (9, 'GMC', 'Rally Wagon G3500', 1995, '1G6DK5EG3A0002795', 'Goldenrod', 115080, 'Pending', 4450730);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (10, 'Toyota', 'Camry Hybrid', 2009, 'WAUMV94E69N782265', 'Blue', 105686, 'Available', 1801777);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (11, 'Saturn', 'L-Series', 2003, '4T3BA3BB0DU948809', 'Aquamarine', 145673, 'Pending', 4798617);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (12, 'Nissan', 'Quest', 1994, 'WAUBH54B11N986571', 'Purple', 184154, 'Available', 1062670);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (13, 'Chevrolet', 'Malibu', 1999, '2B3CK5CT3AH005900', 'Aquamarine', 47828, 'Available', 196845);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (14, 'Chevrolet', 'Express', 2012, '1HGCP2E85AA474865', 'Green', 36600, 'Sold', 4629959);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (15, 'Chevrolet', 'Express 1500', 2004, 'SCBCC41N39C366748', 'Puce', 31579, 'Available', 3701484);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (16, 'Acura', 'Vigor', 1992, 'ZHWGU5BZ5DL082265', 'Purple', 37941, 'Pending', 301560);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (17, 'Lincoln', 'Navigator', 2004, '1G4HR54K024586725', 'Pink', 132208, 'Available', 3344673);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (18, 'Nissan', 'Maxima', 2006, '1G6DE8EY1B0827706', 'Mauv', 52416, 'Available', 1402327);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (19, 'GMC', 'Savana Cargo Van', 2006, '1G6DA1E35C0582790', 'Red', 68328, 'Sold', 185438);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (20, 'Dodge', 'Viper', 1999, 'KNDMG4C70C6798126', 'Yellow', 125479, 'Sold', 4918170);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (21, 'Cadillac', 'XLR', 2007, '1G6KD57Y68U765856', 'Blue', 68388, 'Available', 747162);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (22, 'Audi', 'Coupe GT', 1985, '1G6AE5S3XF0503539', 'Teal', 169322, 'Available', 4629693);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (23, 'Honda', 'Passport', 1998, 'JTDBT4K3XC1094417', 'Orange', 19685, 'Sold', 4430904);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (24, 'Dodge', 'Ram', 2011, 'JTHBK1GG3E2805156', 'Yellow', 148648, 'Available', 1544695);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (25, 'Lincoln', 'Town Car', 2004, '1G4GH5E39CF012300', 'Orange', 69792, 'Available', 1768525);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (26, 'Chevrolet', 'Silverado 2500', 2011, '1N6AD0CW4EN682350', 'Puce', 14714, 'Available', 3388472);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (27, 'GMC', 'Suburban 2500', 1993, '5TFCW5F15BX562575', 'Crimson', 177858, 'Pending', 3565123);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (28, 'Isuzu', 'Rodeo', 1999, '3D4PG4FB2AT605557', 'Maroon', 75415, 'Available', 941242);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (29, 'Buick', 'LeSabre', 2005, '2C3CDXFG9DH085375', 'Puce', 83595, 'Available', 2412406);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (30, 'Mercedes-Benz', 'E-Class', 2001, '1D7RB1CP2AS101262', 'Red', 19327, 'Available', 1878233);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (31, 'Nissan', 'Cube', 2010, '1FMJK1K53AE063071', 'Purple', 144104, 'Available', 2493340);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (32, 'Volkswagen', 'New Beetle', 2004, 'WAULT58E54A763291', 'Pink', 25460, 'Available', 2374641);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (33, 'Plymouth', 'Breeze', 1998, 'WBAPM7C55AE851604', 'Khaki', 135524, 'Available', 1130410);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (34, 'Fillmore', 'Fillmore', 1960, '1FBNE3BL6BD939795', 'Yellow', 33221, 'Pending', 1237755);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (35, 'Dodge', 'Ram 1500', 2006, '1J4PR4GK6AC932958', 'Crimson', 116625, 'Available', 4407776);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (36, 'Toyota', 'Tacoma', 2003, 'WAUCFAFH3AN171129', 'Red', 199880, 'Sold', 1136002);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (37, 'Aptera', 'Typ-1', 2008, '1N6AA0EC9FN326053', 'Turquoise', 80833, 'Sold', 2321356);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (38, 'Volkswagen', 'Fox', 1989, 'JN8AS1MU5CM450472', 'Orange', 81792, 'Sold', 577759);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (39, 'Pontiac', 'Grand Prix', 2007, '1N4AB7AP4DN740748', 'Crimson', 3107, 'Available', 3878025);
+insert into Cars (car_id, brand, model, manufactured_year, VIN, color, mileage, status, price) values (40, 'Bentley', 'Continental', 2008, '3D73M3CL2AG895339', 'Khaki', 79724, 'Sold', 4239922);
+
+insert into Customers (customer_id, name, contact_number, email, address) values (1, 'Gus Morgue', '866-399-6827', 'gmorgue0@microsoft.com', '11844 Dorton Court');
+insert into Customers (customer_id, name, contact_number, email, address) values (2, 'Ichabod Phalip', '613-145-9458', 'iphalip1@skype.com', '7 Nelson Parkway');
+insert into Customers (customer_id, name, contact_number, email, address) values (3, 'Xever Haton', '909-420-6265', 'xhaton2@cam.ac.uk', '00 Raven Place');
+insert into Customers (customer_id, name, contact_number, email, address) values (4, 'Chas Clousley', '284-235-1722', 'cclousley3@rakuten.co.jp', '21 Warner Park');
+insert into Customers (customer_id, name, contact_number, email, address) values (5, 'Titos Ellum', '434-458-1400', 'tellum4@netvibes.com', '43 Blackbird Avenue');
+insert into Customers (customer_id, name, contact_number, email, address) values (6, 'Marylin Trim', '151-592-3957', 'mtrim5@live.com', '96 Grayhawk Pass');
+insert into Customers (customer_id, name, contact_number, email, address) values (7, 'Halette Plank', '119-721-0653', 'hplank6@parallels.com', '38506 Sheridan Park');
+insert into Customers (customer_id, name, contact_number, email, address) values (8, 'Phil Stodhart', '886-750-6078', 'pstodhart7@cocolog-nifty.com', '637 Summerview Drive');
+insert into Customers (customer_id, name, contact_number, email, address) values (9, 'Oran Goldie', '678-487-5568', 'ogoldie8@unc.edu', '07 1st Junction');
+insert into Customers (customer_id, name, contact_number, email, address) values (10, 'Michaelina Blaase', '804-744-3837', 'mblaase9@google.com.br', '937 Lotheville Street');
+insert into Customers (customer_id, name, contact_number, email, address) values (11, 'Peadar Spolton', '750-859-9390', 'pspoltona@list-manage.com', '4 Longview Crossing');
+insert into Customers (customer_id, name, contact_number, email, address) values (12, 'Wileen Kyngdon', '369-100-8403', 'wkyngdonb@illinois.edu', '6996 Northport Place');
+insert into Customers (customer_id, name, contact_number, email, address) values (13, 'Claus Parkey', '926-690-9992', 'cparkeyc@pen.io', '80933 Basil Circle');
+insert into Customers (customer_id, name, contact_number, email, address) values (14, 'Tressa Ricca', '616-962-1429', 'triccad@bravesites.com', '60 Sauthoff Parkway');
+insert into Customers (customer_id, name, contact_number, email, address) values (15, 'Deva Marshman', '914-841-0406', 'dmarshmane@deliciousdays.com', '13628 Dennis Trail');
+insert into Customers (customer_id, name, contact_number, email, address) values (16, 'Natka Ocklin', '820-491-2102', 'nocklinf@typepad.com', '519 Manufacturers Hill');
+insert into Customers (customer_id, name, contact_number, email, address) values (17, 'Hercule Lassell', '495-427-2534', 'hlassellg@list-manage.com', '960 Bashford Plaza');
+insert into Customers (customer_id, name, contact_number, email, address) values (18, 'Dede Bygraves', '237-120-9766', 'dbygravesh@typepad.com', '3 Lyons Place');
+insert into Customers (customer_id, name, contact_number, email, address) values (19, 'Angel Calway', '238-356-4159', 'acalwayi@123-reg.co.uk', '78 Thierer Court');
+insert into Customers (customer_id, name, contact_number, email, address) values (20, 'Roxana MacAlpyne', '810-471-7940', 'rmacalpynej@cornell.edu', '01 Arkansas Street');
+
+
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (1, 3, '06/17/2023', 'Belozërsk');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (2, 5, '07/27/2023', 'Khovd');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (3, 40, '06/08/2023', 'Debrecen');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (4, 19, '10/20/2023', 'Hegarmulya');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (5, 28, '09/19/2023', 'Muang Pakxan');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (6, 15, '06/11/2022', 'Kenyau');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (7, 31, '02/09/2024', 'Al Mughayyir');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (8, 24, '07/09/2023', 'Biao');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (9, 28, '05/01/2022', 'Belfast');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (10, 13, '01/11/2022', 'New York City');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (11, 30, '11/19/2023', 'Brodarica');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (12, 12, '04/24/2024', 'Zhuanqiang');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (13, 33, '04/19/2023', 'Qiping');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (14, 34, '07/24/2023', 'Esperança');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (15, 22, '04/22/2022', 'Dąbrowa');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (16, 11, '07/10/2022', 'Changsu');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (17, 27, '06/30/2023', 'Poitiers');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (18, 36, '01/07/2024', 'Sultan Kudarat');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (19, 34, '10/16/2022', 'Penza');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (20, 11, '11/24/2022', 'Karlskoga');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (21, 31, '07/14/2024', 'Mantampay');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (22, 34, '08/27/2024', 'Morelos');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (23, 26, '01/24/2023', 'Gonābād');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (24, 26, '05/16/2024', 'Dingshan');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (25, 24, '10/07/2022', 'Boljoon');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (26, 15, '11/15/2023', 'Banraeaba Village');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (27, 20, '07/17/2024', 'Ostroróg');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (28, 39, '01/24/2024', 'Yermolino');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (29, 6, '01/06/2023', 'Songshu');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (30, 35, '04/30/2024', 'Waru');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (31, 23, '04/15/2024', 'Tarimbang');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (32, 21, '03/21/2022', 'Wuda');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (33, 37, '01/30/2023', 'Velké Němčice');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (34, 8, '01/29/2022', 'Masvingo');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (35, 14, '11/07/2023', 'Reno');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (36, 27, '10/23/2023', 'Vilhelmina');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (37, 33, '11/09/2023', 'Kuanfeu');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (38, 4, '03/27/2024', 'Jūybār');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (39, 18, '07/06/2024', 'Huanan');
+INSERT INTO Repossession (repo_id, car_id, repo_date, repo_location) VALUES (40, 29, '05/09/2024', 'Mañazo');
+
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (1, 19, 7, 2, '08/01/2024', 185438);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (2, 14, 20, 3, '12/12/2022', 4629959);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (3, 1, 8, 5, '03/21/2024', 1829490);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (4, 40, 18, 5, '07/31/2023', 4239922);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (5, 38, 15, 1, '11/09/2023', 577759);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (6, 20, 13, 4, '01/19/2023', 4918170);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (7, 36, 11, 5, '04/20/2023', 1136002);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (8, 23, 3, 2, '09/05/2024', 4430904);
+insert into Sales (sale_id, car_id, customer_id, user_id, sale_date, sale_price) values (9, 37, 2, 1, '10/02/2023', 2321356);
+
+
+insert into Users (user_id, username, user_password, role) values (1, 'rhauxwell0', 'pG4QaK@s.e*e', 'Repo Officer');
+insert into Users (user_id, username, user_password, role) values (2, 'jsauter1', 'kB0Y4qQqq', 'Repo Officer');
+insert into Users (user_id, username, user_password, role) values (3, 'jvarvell2', 'rH2D0w~fg/$(', 'Admin');
+insert into Users (user_id, username, user_password, role) values (4, 'bstanley3', 'kK4r&ZfWy6', 'Repo Officer');
+insert into Users (user_id, username, user_password, role) values (5, 'shedge4', 'nM3wEE8rd{&/Xq', 'Repo Officer');
+
+
+
+
+
+
+
+
+
+
+
